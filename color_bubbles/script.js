c55a3d27901bb140aaad799cfd8e93c88181e70d
mdabreu <35778755+mdabreu@users.noreply.github.com>
2022-06-13 14:42:38 -0400

    Update script.js

--- color_bubbles/script.js
+++ color_bubbles/script.js
@@ -5,7 +5,22 @@ let blue;
 let randomRGB;
 
 
+function randomcolor() {
+    return Math.floor(Math.random() * 256)
+}
 
+red = Math.floor(Math.random() * 256);
+green = Math.floor(Math.random() * 256);
+blue = Math.floor(Math.random() * 256);
+randomRGB = `rgb( ${red}, ${green}, ${blue} )`;
+
+
+for (let i = 0; i < 11; i++ ) {
+	 html += `<div style="background-color: rgb(${randomcolor()}, ${randomcolor()}, ${randomcolor()})">${i}</div>`;
+     
+}
+
+document.querySelector('main').innerHTML = html;
 
 
 // or 
