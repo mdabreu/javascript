bf41836ac743cb361a085210fef61603b972101f
mdabreu <35778755+mdabreu@users.noreply.github.com>
2022-07-12 17:46:01 -0400

    pets js

---
+++ array_objects/pets.js
@@ -0,0 +1,43 @@
+/*
+  Create an array of 'pet' objects.
+  Each object should have the following properties: 
+  name, type, breed, age, and photo
+*/
+
+var pets = [
+    {
+        name: "buck",
+        type: "dog",
+        breed: "aussie",
+        age: 4,
+        photo: "img/aussie.jpg"
+    },
+    {
+        name: "wheat",
+        type: "dog",
+        breed: "dachshund",
+        age: 8,
+        photo: "img/dachshund.jpg"
+    },
+    {
+        name: "dill",
+        type: "dog",
+        breed: "golden",
+        age: 1,
+        photo: "img/golden.jpg"
+    },
+    {
+        name: "psps",
+        type: "dog",
+        breed: "pug",
+        age: 3,
+        photo: "img/pug.jpg"
+    },
+    {
+        name: "qyil",
+        type: "cat",
+        breed: "persian",
+        age: 9,
+        photo: "img/persian.jpg"
+    }
+];
