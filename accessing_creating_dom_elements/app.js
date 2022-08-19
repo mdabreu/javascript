57e685ac03ddfc298ed9b4382d44d42074e2e2d3
mdabreu <35778755+mdabreu@users.noreply.github.com>
2022-08-19 16:41:09 -0400

    Update app.js

--- accessing_creating_dom_elements/app.js
+++ accessing_creating_dom_elements/app.js
@@ -11,3 +11,15 @@ btnCreate.addEventListener('click', () => {
 });
 
 
+
+### or using insertAdjacentHTML
+
+btnCreate.addEventListener('click', () => {
+    const input = document.querySelector('.input-main');
+    const list = document.querySelector('ul');
+    
+    list.insertAdjacentHTML(
+        'afterbegin',
+        `<li>${input.value}</li>`);
+    input.value = '';
+});
